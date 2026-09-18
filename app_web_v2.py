7b325786f8c3c6cae37a2514c0febfd8f3f6d690
Matheus Pereira Barreto <104918794+Mblackjack@users.noreply.github.com>
2026-09-18 20:26:33 -0300

    Update title in app_web_v2.py

--- app_web_v2.py
+++ app_web_v2.py
@@ -60,7 +60,7 @@ with col_img:
         st.warning("Imagem não encontrada no diretório do projeto.")
 
 with col_titulo:
-    st.title("Sistema Automático de Registro de Ouvidorias (SARO)")
+    st.title("Sistema Automático de Registro de Ouvidorias")
     st.markdown("*Versão 3.0* | Registro e Gestão de Ouvidorias com auxílio de Inteligência Artificial")
 
 st.divider()
